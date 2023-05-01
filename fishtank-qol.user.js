351ddfb05d41785e78ddfc59619fde71f1b2f9b4
that-mint <47940073+that-mint@users.noreply.github.com>
2023-05-01 11:00:35 +1000

    Cleaning up

--- fishtank-qol.user.js
+++ fishtank-qol.user.js
@@ -9,48 +9,6 @@
 (function() {
     'use strict';
 
-    // Import Font Awesome
-    const fontAwesomeLink = document.createElement('link');
-    fontAwesomeLink.rel = 'stylesheet';
-    fontAwesomeLink.href = 'https://use.fontawesome.com/releases/v5.15.3/css/all.css';
-    document.head.appendChild(fontAwesomeLink);
-
-    // Create the volume icon element
-    const volumeIcon = document.createElement('i');
-    volumeIcon.classList.add('fas', 'fa-volume-up');
-    volumeIcon.style.position = 'fixed';
-    volumeIcon.style.bottom = '20px';
-    volumeIcon.style.right = '20px';
-    volumeIcon.style.fontSize = '30px';
-    volumeIcon.style.cursor = 'pointer';
-    document.body.appendChild(volumeIcon);
-
-    // Create the volume bar element
-    const volumeBar = document.createElement('input');
-    volumeBar.type = 'range';
-    volumeBar.min = '0';
-    volumeBar.max = '1';
-    volumeBar.step = '0.01';
-    volumeBar.value = '0.5';
-    volumeBar.style.position = 'fixed';
-    volumeBar.style.bottom = '60px';
-    volumeBar.style.right = '20px';
-    volumeBar.style.width = '150px';
-    volumeBar.style.display = 'none';
-    document.body.appendChild(volumeBar);
-
-    // Create the audio element
-    const audio = new Audio('https://github.com/that-mint/that-mint.github.io/blob/29002a62d49be1b0097be9eb491b22c7845bdbde/audio.mp3?raw=true');
-    audio.volume = volumeBar.value;
-
-    // Listen for volume icon click events
-    volumeIcon.addEventListener('click', () => {
-        volumeBar.style.display = volumeBar.style.display === 'none' ? 'block' : 'none';
-    });
-
-
-
-
     const chatHassleDivSelector = "html body div#__next main.AppShell_app-shell__slfko div.Chat_chat__Bdojy div.Chat_hassle__rwI57";
    
     const observer = new MutationObserver((mutationsList) => {
